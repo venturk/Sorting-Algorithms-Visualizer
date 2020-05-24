5658104cb244e29ce578f2fcb5096dd1085dcbca
venturk <33842147+venturk@users.noreply.github.com>
2020-05-24 09:22:22 +0300

    Added basic app structure.

---
+++ src/sortingVisualizer/SortingVisualizer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './SortingVisualizer.css';
+
+export default class SortingVisualizer extends React.Component {
+    constructor(properties) {
+        super(properties);
+
+        this.state = {
+            array: [],
+        };
+    }
+
+    componentDidMount() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
